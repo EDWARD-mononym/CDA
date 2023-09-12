144f3b4ff8dcfa0e05fe0d47b75a7f31a9ebf953
Edward <edward.mononym@proton.me>
2023-09-12 13:36:16 +0700

    Clean up main.py

--- main.py
+++ main.py
@@ -39,8 +39,6 @@ def main(args = args):
 
     for scenario in configs["Dataset"]["Scenarios"]:
         feature_extractor, classifier = load_source_model(configs, scenario, device)
-        feature_extractor.to(device)
-        classifier.to(device)
 
         #* Test source model acc & save
         result_matrix = Acc_matrix(scenario)
@@ -54,10 +52,7 @@ def main(args = args):
             writer.close()
 
             #* Load the best model & test acc
-            feature_extractor, classifier = load_best_model(configs, target_id)
-            feature_extractor.to(device)
-            classifier.to(device)
-            
+            feature_extractor, classifier = load_best_model(configs, target_id, device)
             target_accs = test_all_domain(configs["Dataset"]["Dataset_Name"], scenario, feature_extractor, classifier, device)
             result_matrix.update(target_id, target_accs)
 
--- utils/load_models.py
+++ utils/load_models.py
@@ -28,9 +28,12 @@ def load_source_model(configs, scenario, device):
         Path(folder_path).mkdir(parents=True, exist_ok=True)
         feature_extractor, classifier = source_train(feature_extractor, classifier, scenario[0], configs, folder_path, device)
 
+    feature_extractor.to(device)
+    classifier.to(device)
+
     return feature_extractor, classifier
 
-def load_best_model(configs, target_name):
+def load_best_model(configs, target_name, device):
     #* Import the feature extractor & classifier 
     backbone_name, classifier_name = configs["BackboneConfig"]["Backbone"], configs["ClassifierConfig"]["Classifier"]
     imported_backbone = importlib.import_module(f"architecture.{backbone_name}")
@@ -46,5 +49,8 @@ def load_best_model(configs, target_name):
     save_folder = os.path.join(os.getcwd(), f'adapted_models/{configs["Dataset"]["Dataset_Name"]}/{configs["AdaptationConfig"]["Method"]}')
     feature_extractor.load_state_dict(torch.load(os.path.join(save_folder, f"{target_name}_feature.pt")))
     classifier.load_state_dict(torch.load(os.path.join(save_folder, f"{target_name}_classifier.pt")))
+
+    feature_extractor.to(device)
+    classifier.to(device)
 
     return feature_extractor, classifier
