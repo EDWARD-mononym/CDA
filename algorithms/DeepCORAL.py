10c70395d0fc869598a395c3e146aff4fee88cff
Edward <edward.mononym@proton.me>
2023-09-12 14:59:04 +0700

    Update DeepCORAL.py

--- algorithms/DeepCORAL.py
+++ algorithms/DeepCORAL.py
@@ -19,7 +19,6 @@ def DeepCORAL(src_loader, trg_loader, feature_extractor, classifier,
 
         # Test & Save best model
         acc_dict = test_all_domain(datasetname, scenario, feature_extractor, classifier, device)
-        print(acc_dict)
 
         if acc_dict[target_name] > best_acc:
             torch.save(feature_extractor.state_dict(), os.path.join(save_path, f"{target_name}_feature.pt"))
