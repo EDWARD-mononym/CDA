f48c718c6bb37adfe50a3cc9258f02caad1e90ad
Edward <edward.mononym@proton.me>
2023-09-29 10:49:24 +0700

    Update load_models.py

--- utils/load_models.py
+++ utils/load_models.py
@@ -3,8 +3,6 @@ import os
 from pathlib import Path
 import torch
 
-from train.source_train import source_train
-
 def load_source_model(configs, feature_extractor, classifier, scenario, device):
     folder_path = os.path.join(os.getcwd(), f"source_models/{configs['Dataset']['Dataset_Name']}/{configs['BackboneConfig']['Backbone']}")
     feature_extractor_path = os.path.join(folder_path, f"{scenario[0]}_feature.pt")
