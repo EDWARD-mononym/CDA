bd2237b12c7c73857da091c36041fc42cc5fad07
Edward <edward.mononym@proton.me>
2023-11-01 13:37:08 +0700

    Added memory component to cosda

--- algorithms/COSDA.py
+++ algorithms/COSDA.py
@@ -1,6 +1,7 @@
 from itertools import cycle
 import os
 import torch
+from torch.utils.data import DataLoader, TensorDataset, ConcatDataset
 from torch.optim.lr_scheduler import StepLR
 import torch.nn as nn
 from algorithms.BaseAlgo import BaseAlgo
@@ -8,8 +9,8 @@ from scipy.spatial.distance import cdist
 import torch.nn.functional as F
 import numpy as np
 import importlib
+import random
 from utils.create_logger import AverageMeter
-
 from collections import defaultdict
 
 class COSDA(BaseAlgo):
@@ -23,7 +24,6 @@ class COSDA(BaseAlgo):
 
         self.teacher_backbone = backbone_class(configs)
         self.teacher_classifier = classifier_class(configs)
-
 
         self.feature_extractor_optimiser = torch.optim.Adam(
             self.feature_extractor.parameters(),
@@ -39,7 +39,9 @@ class COSDA(BaseAlgo):
         self.fe_lr_scheduler = StepLR(self.feature_extractor_optimiser,   step_size=configs.step_size, gamma=configs.gamma)
         self.classifier_lr_scheduler = StepLR(self.classifier_optimiser,     step_size=configs.step_size, gamma=configs.gamma)
 
+        self.memory = None
         self.configs = configs
+
     def epoch_train(self, src_loader, trg_loader, epoch, device):
         utilized_ratio = AverageMeter()
 
@@ -54,14 +56,25 @@ class COSDA(BaseAlgo):
         self.teacher_backbone.train()
         self.teacher_classifier.train()
 
-
-        combined_loader = zip(cycle(src_loader), trg_loader)
-
         loss_dict = defaultdict(float)
 
-        for step, (source, target) in enumerate(combined_loader):
+        epoch_memory_inputs, epoch_memory_labels = [], []
+
+        if self.memory:
+            combined_loader = zip(cycle(src_loader), trg_loader, cycle(self.memory))
+        else:
+            combined_loader = zip(cycle(src_loader), trg_loader)
+
+        for step, (data) in enumerate(combined_loader):
+            if self.memory:
+                source, target, memory = data
+                mem_x = memory[0].to(device)
+            else:
+                source, target = data
+                memory, mem_x = None, None
+
             # Extract data from source and target batches
-            src_x, src_y, trg_x, trg_idx = source[0], source[1], target[0], target[2]
+            src_x, src_y, trg_x, trg_y = source[0], source[1], target[0], target[1]
             src_x, src_y, trg_x = src_x.to(device), src_y.to(device), trg_x.to(device)
 
             # Reset the gradients
@@ -104,6 +117,49 @@ class COSDA(BaseAlgo):
                 torch.sum(softmax_output * (log_softmax_output - log_margin_output), dim=1)
             )
 
+
+            ###### ADDED REPLAY MEMORY #####
+            if memory:
+                # Obtain teacher's predictions without gradients
+                with torch.no_grad():
+                    predictions = self.teacher_classifier(self.teacher_backbone(mem_x))
+                    knowledge, knowledge_mask = self.distill_knowledge(predictions, self.configs.conf_gate,
+                                                                    temperature= self.configs.temp)
+
+                # Mixup preparation
+                if  self.configs.beta > 0:
+                    lam = np.random.beta( self.configs.beta, self.configs.beta)
+                    lam = max(lam, 1 - lam)  # Ensure lam is closer to 1
+                else:
+                    lam = 1
+
+                batch_size = mem_x.size(0)
+                index = torch.randperm(batch_size).cuda()
+                mixed_image = lam * mem_x + (1 - lam) * mem_x[index, :]
+                mixed_consensus = lam * knowledge + (1 - lam) * knowledge[index, :]
+
+                # Compute consistency loss
+                mixed_output = self.classifier(self.feature_extractor(mixed_image))
+                mixed_log_softmax = torch.log_softmax(mixed_output, dim=1)
+                mem_consistency_loss = torch.sum(
+                    knowledge_mask * torch.sum(-1 * mixed_consensus * mixed_log_softmax, dim=1)
+                ) / torch.sum(knowledge_mask) #! Potential to crash?
+
+                # Compute regularization
+                output = self.classifier(self.feature_extractor(mem_x))
+                softmax_output = torch.softmax(output, dim=1)
+                margin_output = torch.mean(softmax_output, dim=0)
+                log_softmax_output = torch.log_softmax(output, dim=1)
+                log_margin_output = torch.log(margin_output + 1e-5)
+
+                mem_mutual_info_loss = -1 * torch.mean(
+                    torch.sum(softmax_output * (log_softmax_output - log_margin_output), dim=1)
+                )
+
+                consistency_loss += mem_consistency_loss
+                mutual_info_loss += mem_mutual_info_loss
+            ####### END OF REPLAY MEMORY SECTION #####
+
             # Calculate final task loss
             if  self.configs.only_mi:
                 task_loss =  self.configs.reg_alpha * mutual_info_loss
@@ -124,9 +180,35 @@ class COSDA(BaseAlgo):
             loss_dict["avg_mutual_info_loss"] += mutual_info_loss.item() / len(src_x)
             loss_dict["avg_consistency_loss"] += consistency_loss.item() / len(src_x)
 
+            epoch_memory_inputs.append(trg_x.cpu().detach())
+            epoch_memory_labels.append(trg_y.cpu().detach())
+
         #* Adjust learning rate
         self.fe_lr_scheduler.step()
         self.classifier_lr_scheduler.step()
+
+        if epoch == self.configs.n_epoch-1:
+            # Select a portion of the current data for the memory
+            indices = list(range(len(epoch_memory_inputs)))
+            random.shuffle(indices)
+            # n_to_store = int(self.configs.alpha * len(epoch_memory_inputs))
+            n_to_store = int(0.05 * len(epoch_memory_inputs))
+            selected_indices = indices[:n_to_store]
+
+            selected_inputs = [epoch_memory_inputs[i] for i in selected_indices]
+            selected_labels = [epoch_memory_inputs[i] for i in selected_indices]
+            
+            selected_inputs = torch.cat(selected_inputs)
+            selected_labels = torch.cat(selected_labels)
+
+            new_memory = DataLoader(TensorDataset(selected_inputs, selected_labels), batch_size=trg_loader.batch_size)
+
+            # Update memory
+            if self.memory is None:
+                self.memory = new_memory
+            else:
+                concatenated_dataset = ConcatDataset([self.memory.dataset, new_memory.dataset])
+                self.memory = DataLoader(concatenated_dataset, batch_size=trg_loader.batch_size)
 
         return loss_dict
 
