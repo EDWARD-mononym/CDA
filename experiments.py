a82933ee7556447cf791cdbaa68daaf92fc7eedd
Edward <edward.mononym@proton.me>
2024-01-23 09:30:54 +0700

    Update experiments.py

--- experiments.py
+++ experiments.py
@@ -43,40 +43,48 @@ class FDTrain(DomainTrainer):
     def save_run_results(self, scenario, run):
         """Save the results after training and adaptation."""
         self.evaluator.calc_metric()
-        file_name = os.path.join(os.getcwd(), f'results/{self.configs.Dataset_Name}/{self.args.experiment}/{self.args.algo}/{scenario}/Run_{run}')
+        file_name = os.path.join(os.getcwd(), f'experiment_results/{self.configs.Dataset_Name}/{self.args.experiment}/{self.args.algo}/{scenario}/Run_{run}')
         if not os.path.exists(file_name):
             os.makedirs(file_name)
         self.evaluator.save_singlerun(file_name)
 
+    def list_algos(self):
+        path = os.path.join(os.getcwd(), "algorithms", "experiments", self.args.experiment)
+        return [f[:-3] for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith('.py')]
+
     def train(self):
-        """Handle all scenarios for training and adaptation."""
-        for scenario in self.configs.Scenarios:
-            source_name = scenario[0]
+        #? Get all the algorithm
+        algolist = self.list_algos()
+        for algo in algolist:
+            self.args.algo = algo
+            """Handle all scenarios for training and adaptation."""
+            for scenario in self.configs.Scenarios:
+                source_name = scenario[0]
 
-            # Initialize evaluator matrix
-            self.evaluator = DomainEvaluator(self.device, scenario, self.configs)
+                # Initialize evaluator matrix
+                self.evaluator = DomainEvaluator(self.device, scenario, self.configs)
 
-            print("===============================================")
-            print("                   CONFIG INFO                  ")
-            print("===============================================")
-            print(f"Method: {self.configs.Method}")
-            print(f"Dataset: {self.configs.Dataset_Name}")
-            print(f"Scenario: {' → '.join(scenario)}")
-            print("===============================================")
+                print("===============================================")
+                print("                   CONFIG INFO                  ")
+                print("===============================================")
+                print(f"Method: {self.configs.Method}")
+                print(f"Dataset: {self.configs.Dataset_Name}")
+                print(f"Scenario: {' → '.join(scenario)}")
+                print("===============================================")
 
-            for run in range(args.Nruns):
-                set_seed(run)
+                for run in range(args.Nruns):
+                    set_seed(run)
 
-                # Train source model and log performance
-                self.train_and_log_source_model(source_name, scenario)
+                    # Train source model and log performance
+                    self.train_and_log_source_model(source_name, scenario)
 
-                # Adapt to all target domains
-                self.adapt_to_target_domains(scenario)
+                    # Adapt to all target domains
+                    self.adapt_to_target_domains(scenario)
 
-                # Calculate metrics and save results
-                self.save_run_results(scenario, run)
+                    # Calculate metrics and save results
+                    self.save_run_results(scenario, run)
 
-            self.save_avg_runs(scenario)
+                self.save_avg_runs(scenario)
 
 
 def parse_arguments():
@@ -88,8 +96,6 @@ def parse_arguments():
     parser.add_argument('--start-domain', default=0, type=int, help='Manual domain start.')
     # ======== Select the experiment =============
     parser.add_argument("--experiment", default="Ablation", help="Experiments available: Ablation, Replay, Stability")
-    # ========= Select the algoritm ==============
-    parser.add_argument("--algo", default="EverAdapt", help="Algorithm to use: CDAN, DANN, DeepCORAL, DSAN, MMDA")
     # ========= Experiment settings ===============
     parser.add_argument("--writer", default="tensorboard", choices=["tensorboard", "wandb"], help="Logging tool to use.")
     parser.add_argument('-lp', '--log-path', default="./logs", help="Path for logging.")
